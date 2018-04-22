28576f1167c059239367bc4d76470a736b27f25c
Mohit Darmwal <36398566+mickmohit@users.noreply.github.com>
2018-04-22 21:45:03 +0530

    Add files via upload

---
+++ mohitspringboot/src/main/java/io/mohitspringboot/topic/TopicService.java
@@ -0,0 +1,52 @@
+package io.mohitspringboot.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	
+	private List<Topic> topics= new ArrayList<>( Arrays.asList(
+			new Topic("spring","spring framework","SFD"),
+			new Topic("Java","Core Java framework","SFD"),
+			new Topic("Javascript","JS framework","SFD")
+			));
+	
+	public List<Topic> getAllTopic()
+	{
+		return topics;
+	}
+
+	public Topic getTopic(String id)
+	{
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		// TODO Auto-generated method stub
+	topics.add(topic);	
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<topics.size();i++)
+		{
+			Topic t=topics.get(i);
+			if(t.getId().equals(id))
+			{
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		// TODO Auto-generated method stub
+		topics.removeIf(t->t.getId().equals(id));
+	}
+	
+}
